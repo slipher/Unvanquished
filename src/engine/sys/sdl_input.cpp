d5c12d482876b8cf1371f244080f1f972045e434
Your Name <you@example.com>
2014-05-23 10:55:59 -0500

    minimal keyboard fix

--- src/engine/sys/sdl_input.cpp
+++ src/engine/sys/sdl_input.cpp
@@ -295,282 +295,319 @@ static qboolean IN_IsConsoleKey( keyNum_t key, const unsigned char character )
 IN_TranslateSDLToQ3Key
 ===============
 */
-static keyNum_t IN_TranslateSDLToQ3Key( SDL_Keysym *keysym, qboolean down )
+static keyNum_t IN_TranslateSDLToQ3Key(SDL_Keysym *keysym, qboolean down)
 {
-	keyNum_t key = (keyNum_t) 0;
+	keyNum_t key = (keyNum_t)0;
 
 #if !SDL_VERSION_ATLEAST( 2, 0, 0 )
 	key = (keyNum_t) keysym->unicode;
 #endif
 
-	if ( keysym->sym >= SDLK_SPACE && keysym->sym < SDLK_DELETE )
+	if (keysym->sym >= SDLK_SPACE && keysym->sym < SDLK_DELETE)
 	{
-		// These happen to match the ASCII chars
-		key = ( keyNum_t ) keysym->sym;
+		/*// These happen to match the ASCII chars
+		key = ( keyNum_t ) keysym->sym;*/
+		if (keysym->scancode >= SDL_SCANCODE_A && keysym->scancode <= SDL_SCANCODE_Z)
+		{
+			key = (keyNum_t)(keysym->scancode - SDL_SCANCODE_A + 'a');
+		}
+		else switch (keysym->scancode)
+		{
+		case SDL_SCANCODE_SEMICOLON:
+			key = (keyNum_t)';';
+			break;
+		case SDL_SCANCODE_MINUS:
+			key = (keyNum_t)'-';
+			break;
+		case SDL_SCANCODE_EQUALS:
+			key = (keyNum_t)'=';
+			break;
+		case SDL_SCANCODE_LEFTBRACKET:
+			key = (keyNum_t)'[';
+			break;
+		case SDL_SCANCODE_RIGHTBRACKET:
+			key = (keyNum_t)']';
+			break;
+		case SDL_SCANCODE_APOSTROPHE:
+			key = (keyNum_t)'\'';
+			break;
+		case SDL_SCANCODE_PERIOD:
+			key = (keyNum_t)'.';
+			break;
+		case SDL_SCANCODE_COMMA:
+			key = (keyNum_t)',';
+			break;
+		case SDL_SCANCODE_SLASH:
+			key = (keyNum_t)'/';
+			break;
+		default:
+			key = (keyNum_t)keysym->sym;
+		}
 	}
 	else
 	{
-		switch ( keysym->sym )
+
+		switch (keysym->sym)
 		{
-			case SDLK_PAGEUP:
-				key = K_PGUP;
-				break;
+		case SDLK_PAGEUP:
+			key = K_PGUP;
+			break;
 
-			case SDLK_KP_9:
-				key = K_KP_PGUP;
-				break;
+		case SDLK_KP_9:
+			key = K_KP_PGUP;
+			break;
 
-			case SDLK_PAGEDOWN:
-				key = K_PGDN;
-				break;
+		case SDLK_PAGEDOWN:
+			key = K_PGDN;
+			break;
 
-			case SDLK_KP_3:
-				key = K_KP_PGDN;
-				break;
+		case SDLK_KP_3:
+			key = K_KP_PGDN;
+			break;
 
-			case SDLK_KP_7:
-				key = K_KP_HOME;
-				break;
+		case SDLK_KP_7:
+			key = K_KP_HOME;
+			break;
 
-			case SDLK_HOME:
-				key = K_HOME;
-				break;
+		case SDLK_HOME:
+			key = K_HOME;
+			break;
 
-			case SDLK_KP_1:
-				key = K_KP_END;
-				break;
+		case SDLK_KP_1:
+			key = K_KP_END;
+			break;
 
-			case SDLK_END:
-				key = K_END;
-				break;
+		case SDLK_END:
+			key = K_END;
+			break;
 
-			case SDLK_KP_4:
-				key = K_KP_LEFTARROW;
-				break;
+		case SDLK_KP_4:
+			key = K_KP_LEFTARROW;
+			break;
 
-			case SDLK_LEFT:
-				key = K_LEFTARROW;
-				break;
+		case SDLK_LEFT:
+			key = K_LEFTARROW;
+			break;
 
-			case SDLK_KP_6:
-				key = K_KP_RIGHTARROW;
-				break;
+		case SDLK_KP_6:
+			key = K_KP_RIGHTARROW;
+			break;
 
-			case SDLK_RIGHT:
-				key = K_RIGHTARROW;
-				break;
+		case SDLK_RIGHT:
+			key = K_RIGHTARROW;
+			break;
 
-			case SDLK_KP_2:
-				key = K_KP_DOWNARROW;
-				break;
+		case SDLK_KP_2:
+			key = K_KP_DOWNARROW;
+			break;
 
-			case SDLK_DOWN:
-				key = K_DOWNARROW;
-				break;
+		case SDLK_DOWN:
+			key = K_DOWNARROW;
+			break;
 
-			case SDLK_KP_8:
-				key = K_KP_UPARROW;
-				break;
+		case SDLK_KP_8:
+			key = K_KP_UPARROW;
+			break;
 
-			case SDLK_UP:
-				key = K_UPARROW;
-				break;
+		case SDLK_UP:
+			key = K_UPARROW;
+			break;
 
-			case SDLK_ESCAPE:
-				key = K_ESCAPE;
-				break;
+		case SDLK_ESCAPE:
+			key = K_ESCAPE;
+			break;
 
-			case SDLK_KP_ENTER:
-				key = K_KP_ENTER;
-				break;
+		case SDLK_KP_ENTER:
+			key = K_KP_ENTER;
+			break;
 
-			case SDLK_RETURN:
-				key = K_ENTER;
-				break;
+		case SDLK_RETURN:
+			key = K_ENTER;
+			break;
 
-			case SDLK_TAB:
-				key = K_TAB;
-				break;
+		case SDLK_TAB:
+			key = K_TAB;
+			break;
 
-			case SDLK_F1:
-				key = K_F1;
-				break;
+		case SDLK_F1:
+			key = K_F1;
+			break;
 
-			case SDLK_F2:
-				key = K_F2;
-				break;
+		case SDLK_F2:
+			key = K_F2;
+			break;
 
-			case SDLK_F3:
-				key = K_F3;
-				break;
+		case SDLK_F3:
+			key = K_F3;
+			break;
 
-			case SDLK_F4:
-				key = K_F4;
-				break;
+		case SDLK_F4:
+			key = K_F4;
+			break;
 
-			case SDLK_F5:
-				key = K_F5;
-				break;
+		case SDLK_F5:
+			key = K_F5;
+			break;
 
-			case SDLK_F6:
-				key = K_F6;
-				break;
+		case SDLK_F6:
+			key = K_F6;
+			break;
 
-			case SDLK_F7:
-				key = K_F7;
-				break;
+		case SDLK_F7:
+			key = K_F7;
+			break;
 
-			case SDLK_F8:
-				key = K_F8;
-				break;
+		case SDLK_F8:
+			key = K_F8;
+			break;
 
-			case SDLK_F9:
-				key = K_F9;
-				break;
+		case SDLK_F9:
+			key = K_F9;
+			break;
 
-			case SDLK_F10:
-				key = K_F10;
-				break;
+		case SDLK_F10:
+			key = K_F10;
+			break;
 
-			case SDLK_F11:
-				key = K_F11;
-				break;
+		case SDLK_F11:
+			key = K_F11;
+			break;
 
-			case SDLK_F12:
-				key = K_F12;
-				break;
+		case SDLK_F12:
+			key = K_F12;
+			break;
 
-			case SDLK_F13:
-				key = K_F13;
-				break;
+		case SDLK_F13:
+			key = K_F13;
+			break;
 
-			case SDLK_F14:
-				key = K_F14;
-				break;
+		case SDLK_F14:
+			key = K_F14;
+			break;
 
-			case SDLK_F15:
-				key = K_F15;
-				break;
+		case SDLK_F15:
+			key = K_F15;
+			break;
 
-			case SDLK_BACKSPACE:
-				key = K_BACKSPACE;
-				break;
+		case SDLK_BACKSPACE:
+			key = K_BACKSPACE;
+			break;
 
-			case SDLK_KP_PERIOD:
-				key = K_KP_DEL;
-				break;
+		case SDLK_KP_PERIOD:
+			key = K_KP_DEL;
+			break;
 
-			case SDLK_DELETE:
-				key = K_DEL;
-				break;
+		case SDLK_DELETE:
+			key = K_DEL;
+			break;
 
-			case SDLK_PAUSE:
-				key = K_PAUSE;
-				break;
+		case SDLK_PAUSE:
+			key = K_PAUSE;
+			break;
 
-			case SDLK_LSHIFT:
-			case SDLK_RSHIFT:
-				key = K_SHIFT;
-				break;
+		case SDLK_LSHIFT:
+		case SDLK_RSHIFT:
+			key = K_SHIFT;
+			break;
 
-			case SDLK_LCTRL:
-			case SDLK_RCTRL:
-				key = K_CTRL;
-				break;
+		case SDLK_LCTRL:
+		case SDLK_RCTRL:
+			key = K_CTRL;
+			break;
 
-			case SDLK_RGUI:
-			case SDLK_LGUI:
-				key = K_COMMAND;
-				break;
+		case SDLK_RGUI:
+		case SDLK_LGUI:
+			key = K_COMMAND;
+			break;
 
-			case SDLK_RALT:
-			case SDLK_LALT:
-				key = K_ALT;
-				break;
+		case SDLK_RALT:
+		case SDLK_LALT:
+			key = K_ALT;
+			break;
 
-			case SDLK_KP_5:
-				key = K_KP_5;
-				break;
+		case SDLK_KP_5:
+			key = K_KP_5;
+			break;
 
-			case SDLK_INSERT:
-				key = K_INS;
-				break;
+		case SDLK_INSERT:
+			key = K_INS;
+			break;
 
-			case SDLK_KP_0:
-				key = K_KP_INS;
-				break;
+		case SDLK_KP_0:
+			key = K_KP_INS;
+			break;
 
-			case SDLK_KP_MULTIPLY:
-				key = K_KP_STAR;
-				break;
+		case SDLK_KP_MULTIPLY:
+			key = K_KP_STAR;
+			break;
 
-			case SDLK_KP_PLUS:
-				key = K_KP_PLUS;
-				break;
+		case SDLK_KP_PLUS:
+			key = K_KP_PLUS;
+			break;
 
-			case SDLK_KP_MINUS:
-				key = K_KP_MINUS;
-				break;
+		case SDLK_KP_MINUS:
+			key = K_KP_MINUS;
+			break;
 
-			case SDLK_KP_DIVIDE:
-				key = K_KP_SLASH;
-				break;
+		case SDLK_KP_DIVIDE:
+			key = K_KP_SLASH;
+			break;
 
-			case SDLK_MODE:
-				key = K_MODE;
-				break;
+		case SDLK_MODE:
+			key = K_MODE;
+			break;
 
-			case SDLK_HELP:
-				key = K_HELP;
-				break;
+		case SDLK_HELP:
+			key = K_HELP;
+			break;
 
-			case SDLK_PRINTSCREEN:
-				key = K_PRINT;
-				break;
+		case SDLK_PRINTSCREEN:
+			key = K_PRINT;
+			break;
 
-			case SDLK_SYSREQ:
-				key = K_SYSREQ;
-				break;
+		case SDLK_SYSREQ:
+			key = K_SYSREQ;
+			break;
 
-			case SDLK_MENU:
-				key = K_MENU;
-				break;
+		case SDLK_MENU:
+			key = K_MENU;
+			break;
 
-			case SDLK_APPLICATION:
-				key = K_COMPOSE;
-				break;
+		case SDLK_APPLICATION:
+			key = K_COMPOSE;
+			break;
 
-			case SDLK_POWER:
-				key = K_POWER;
-				break;
+		case SDLK_POWER:
+			key = K_POWER;
+			break;
 
-			case SDLK_UNDO:
-				key = K_UNDO;
-				break;
+		case SDLK_UNDO:
+			key = K_UNDO;
+			break;
 
-			case SDLK_SCROLLLOCK:
-				key = K_SCROLLOCK;
-				break;
+		case SDLK_SCROLLLOCK:
+			key = K_SCROLLOCK;
+			break;
 
-			case SDLK_NUMLOCKCLEAR:
-				key = K_KP_NUMLOCK;
-				break;
+		case SDLK_NUMLOCKCLEAR:
+			key = K_KP_NUMLOCK;
+			break;
 
-			case SDLK_CAPSLOCK:
-				key = K_CAPSLOCK;
-				break;
+		case SDLK_CAPSLOCK:
+			key = K_CAPSLOCK;
+			break;
 
-			default:
-				break;
+		default:
+			break;
 		}
 	}
 
-	if ( in_keyboardDebug->integer )
+	if (in_keyboardDebug->integer)
 	{
-		IN_PrintKey( keysym, key, down );
+		IN_PrintKey(keysym, key, down);
 	}
 
-	if ( IN_IsConsoleKey( key, 0 ) && !keys[ K_ALT ].down)
+	if (IN_IsConsoleKey(key, 0) && !keys[K_ALT].down)
 	{
 		// Console keys can't be bound or generate characters
 		// (but allow Alt+key for text input)
@@ -579,7 +616,6 @@ static keyNum_t IN_TranslateSDLToQ3Key( SDL_Keysym *keysym, qboolean down )
 
 	return key;
 }
-
 /*
 ===============
 IN_GobbleMotionEvents
