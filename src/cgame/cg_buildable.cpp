cd8352d3a8e3a7b0c791cce695c0ab11f1173990
slipher <slipher@protonmail.com>
2021-07-15 20:23:43 -0500

    Skip loading some buildables/classes/weapons

--- src/cgame/cg_buildable.cpp
+++ src/cgame/cg_buildable.cpp
@@ -618,7 +618,7 @@ void CG_InitBuildables()
 
 	suffixes.push_back( "" );
 
-	for ( buildable = (buildable_t)( BA_NONE + 1 ); buildable < BA_NUM_BUILDABLES;
+	for ( buildable = (buildable_t)( BA_H_SPAWN ); buildable < BA_NUM_BUILDABLES;
 	      buildable = (buildable_t)( buildable + 1 ) )
 	{
 		buildableInfo_t *bi = &cg_buildables[ buildable ];
