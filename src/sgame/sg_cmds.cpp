a6c47acf15bbcffc35b922fdcc672ea34259fb75
slipher <slipher@protonmail.com>
2021-09-05 22:16:48 -0500

    bash round stuff

--- src/sgame/sg_cmds.cpp
+++ src/sgame/sg_cmds.cpp
@@ -4286,8 +4286,8 @@ static void Cmd_ReplyPrivateMessage_f( gentity_t *ent )
 
 void Cmd_BashRound( gentity_t* )
 {
-	extern bool bashRound;
-	bashRound = true;
+	void SetUpBashRound();
+	SetUpBashRound();
 }
 
 // commands must be in alphabetical order!
