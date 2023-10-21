2f805c84205330c54675c37d66b87171efc4c8fb
slipher <slipher@protonmail.com>
2023-10-21 20:11:56 -0300

    Bots avoid green crates

--- src/sgame/components/GreenCrateComponent.cpp
+++ src/sgame/components/GreenCrateComponent.cpp
@@ -2,8 +2,15 @@
 
 GreenCrateComponent::GreenCrateComponent(Entity& entity, HealthComponent& r_HealthComponent, ThinkingComponent& r_ThinkingComponent)
 	: GreenCrateComponentBase(entity, r_HealthComponent, r_ThinkingComponent)
-{}
+{
+	glm::vec3 mins = VEC2GLM(entity.oldEnt->r.currentOrigin) - 25.f;
+	glm::vec3 maxs = VEC2GLM(entity.oldEnt->r.currentOrigin) + 25.f;
+	mins.z -= 1000;
+	maxs.z += 1000;
+	G_BotAddObstacle(mins, maxs, entity.oldEnt->num());
+}
 
 void GreenCrateComponent::HandleDie(gentity_t* killer, meansOfDeath_t meansOfDeath) {
 	entity.Get<MissileComponent>()->Explode();
+	G_BotRemoveObstacle(entity.oldEnt->num());
 }
